ed8e8544dad165b22b39283012456ea0fcad844e
Aigars Bedeicis <a.bedeicis@gmail.com>
2025-01-26 00:45:42 +0200

    Add Liberation of Undermine first 4 bosses

---
+++ NHFAuraManager/Media.lua
@@ -0,0 +1,23 @@
+local LSM = LibStub("LibSharedMedia-3.0");
+
+-- Fonts
+LSM:Register("font", "Bahnschrift Semi Bold", [[Interface\Addons\NHFAuraManager\Media\Fonts\bahnschrift_semibold.ttf]]);
+LSM:Register("font", "Poppins Bold", [[Interface\Addons\NHFAuraManager\Media\Fonts\Poppins-Bold.ttf]]);
+LSM:Register("font", "Poppins Medium", [[Interface\Addons\NHFAuraManager\Media\Fonts\Poppins-Medium.ttf]]);
+LSM:Register("font", "Poppins SemiBold", [[Interface\Addons\NHFAuraManager\Media\Fonts\Poppins-SemiBold.ttf]]);
+
+-- Sounds
+LSM:Register("sound", "|cffc334ebTaunt - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Taunt.mp3]]);
+LSM:Register("sound", "|cffc334eb1 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\One.mp3]]);
+LSM:Register("sound", "|cffc334eb2 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Two.mp3]]);
+LSM:Register("sound", "|cffc334eb3 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Three.mp3]]);
+LSM:Register("sound", "|cffc334eb4 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Four.mp3]]);
+LSM:Register("sound", "|cffc334eb5 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Five.mp3]]);
+LSM:Register("sound", "|cffc334eb6 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Six.mp3]]);
+LSM:Register("sound", "|cffc334eb7 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Seven.mp3]]);
+LSM:Register("sound", "|cffc334eb8 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Eight.mp3]]);
+LSM:Register("sound", "|cffc334eb9 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Nine.mp3]]);
+LSM:Register("sound", "|cffc334eb10 - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\Ten.mp3]]);
+LSM:Register("sound", "|cffc334ebToo Close - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\TooClose.mp3]]);
+LSM:Register("sound", "|cffc334ebCharge High - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\ChargeHigh.mp3]]);
+LSM:Register("sound", "|cffc334ebMove Boss - NHF|r", [[Interface\Addons\NHFAuraManager\Media\Sounds\MoveBoss.mp3]]);
